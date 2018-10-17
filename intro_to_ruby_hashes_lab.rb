b9159bc05e66d6905559b3231739112b3e32ba11
Tak H Kim <takhyun818@gmail.com>
2018-10-17 21:31:08 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def monopoly
 	monopoly = {:railroads=>
 	{
-	
+
 	}
 }
 
@@ -22,10 +22,14 @@ def monopoly_with_second_tier
 	monopoly = {:railroads=>
 	{
 		:pieces=>4
-		:names=>{}
-		:rent_in_dollars=>{}
-
+		:names=>{
+			
+		}
+		:rent_in_dollars=>{
+			
+		}
 	}
+}
 end
 
 def monopoly_with_third_tier
